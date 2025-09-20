40e62a2d7fd1d7a1af216c125babc122d3c844c9
Himanshuvaid562 <himanshuvaid562@gmail.com>
2025-09-20 09:18:27 +0530

    update for mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,97 +1,97 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SearchNavigator from "./SearchNavigator";
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 
 export default function Navbar() {
   return (
     <motion.div
-            initial={{ opacity: 0, y: 30 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.3 }}
-            className="w-full max-w-lg bg-white rounded-2xl shadow-xl p-8"
+      initial={{ opacity: 0, y: 30 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.3 }}
+      className="w-full max-w-lg bg-white rounded-2xl shadow-xl p-8"
+    >
+      <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
           >
-    <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          Navbar
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0 my-textarea">
-            <li className="nav-item ">
-              <Link className="nav-link active" aria-current="page" to="/">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/About">
-                About Me
-              </Link>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Projects
-              </a>
-              <ul className="dropdown-menu">
-                {/* <li>
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 my-textarea">
+              <li className="nav-item ">
+                <Link className="nav-link active" aria-current="page" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/About">
+                  About Me
+                </Link>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Projects
+                </a>
+                <ul className="dropdown-menu">
+                  {/* <li>
                   <a className="dropdown-item" href="#">
                     Secure Chatting Web App
                   </a>
                 </li> */}
-                <li>
-                  <Link className="dropdown-item" to="./projects/Curr">
-                    Currency Convertor
-                  </Link>
-                </li>
-                <li></li>
-                <li>
-                  <Link to="./projects/tic" className="dropdown-item">
-                    Tic-Tak-Toe
-                  </Link>
-                </li>
-                <li>
-                  <Link to="./projects/rps" className="dropdown-item">
-                    Rock-Paper-Scissors
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <Link to="/resume" className="nav-link">
-                Resume
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">
-                Contact
-              </Link>
-            </li>
-          </ul>
-          <form className="d-flex" role="search">
-            <SearchNavigator />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
+                  <li>
+                    <Link className="dropdown-item" to="./projects/Curr">
+                      Currency Convertor
+                    </Link>
+                  </li>
+                  <li></li>
+                  <li>
+                    <Link to="./projects/tic" className="dropdown-item">
+                      Tic-Tak-Toe
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="./projects/rps" className="dropdown-item">
+                      Rock-Paper-Scissors
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <Link to="/resume" className="nav-link">
+                  Resume
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <SearchNavigator />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
     </motion.div>
   );
 }
--- src/components/ProjectFrame.js
+++ src/components/ProjectFrame.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-function ProjectFrame({file}) {
+function ProjectFrame({ file }) {
     return (
+
+
         <div style={{ width: "100%", height: "100vh", padding: "10px" }}>
-           
+
 
             <iframe src={file} style={{ width: "100%", height: "90vh", border: "none", borderRadius: "10px" }}></iframe>
 
