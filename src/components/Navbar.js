7901697eac234587f07b5cefa67c5303d7224fd4
Himanshuvaid562 <himanshuvaid562@gmail.com>
2025-09-20 09:52:18 +0530

    update for mobile navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import SearchNavigator from "./SearchNavigator";
 import { motion } from 'framer-motion';
+import './style.css';
 
 export default function Navbar() {
   return (
