82b41e5e6288d11e47548099fe4cec53b9f527ca
Himanshuvaid562 <himanshuvaid562@gmail.com>
2025-09-20 12:54:21 +0530

    final proper contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -94,7 +94,7 @@ export default function Contact() {
     className="center block w-full p-5 rounded-lg bg-gray-900 border border-gray-700 text-gray-100 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-teal-400"
     value={message}
     onChange={(e) => setMessage(e.target.value)}
-    style={{justifyContent: "center",alignItems: "center", textAlign: "center", width: '39rem', maxWidth: '55%'}}
+    style={{justifyContent: "center",alignItems: "center", textAlign: "center", width: '39rem', maxWidth: '80%'}}
 
     // display: flex;
     // flex-wrap:wrap;
